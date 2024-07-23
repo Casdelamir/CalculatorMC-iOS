afd26a17dfca77c70294e7ede9007033510d945c
Delamir <casdelamir@gmail.com>
2024-07-23 14:05:29 +0200

    Calculator is done

--- CalculatorMC-iOS/MainViewController.swift
+++ CalculatorMC-iOS/MainViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var valueHight: UILabel!
+    @IBOutlet weak var valueWeight: UILabel!
+    @IBOutlet weak var result: UILabel!
+    
+    var currentValueHight: Float = 0.0
+    var currentValueWeghit: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func setWeight(_ sender: UIStepper) {
+        currentValueWeghit = Float(sender.value)
+        valueWeight.text = "\( currentValueWeghit) kg"
+    }
+    
+    
+    @IBAction func setHight(_ sender: UISlider) {
+        currentValueHight = sender.value
+        valueHight.text = "\(currentValueHight) cm"
+    }
+    
+    @IBAction func calculateResult(_ sender: Any) {
+        var resultNum = currentValueWeghit / pow((currentValueHight / 100.0), 2)
+        result.text = String(format: "%.2f", resultNum)
     }
 }
 
