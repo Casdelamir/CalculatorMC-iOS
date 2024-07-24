a28df34d3f6afae68fdad597c69c240339addfd5
Delamir <casdelamir@gmail.com>
2024-07-24 10:04:19 +0200

    Added alert message

--- CalculatorMC-iOS/MainViewController.swift
+++ CalculatorMC-iOS/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var valueWeight: UILabel!
     @IBOutlet weak var result: UILabel!
     
+    var resultMessage: String!
     var currentValueHight: Float = 0.0
     var currentValueWeghit: Float = 0.0
     
@@ -20,6 +21,11 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+            
+    // Add a default action (OK button)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                print("OK tapped")
+            }
     
 
     @IBAction func setWeight(_ sender: UIStepper) {
@@ -30,12 +36,34 @@ class MainViewController: UIViewController {
     
     @IBAction func setHight(_ sender: UISlider) {
         currentValueHight = sender.value
-        valueHight.text = "\(currentValueHight) cm"
+        valueHight.text = String(format: "%.2f cm", currentValueHight)
     }
     
     @IBAction func calculateResult(_ sender: Any) {
         var resultNum = currentValueWeghit / pow((currentValueHight / 100.0), 2)
         result.text = String(format: "%.2f", resultNum)
+        
+        switch resultNum {
+        case ..<18.5:
+            resultMessage = "Underweight"
+        case 18.5...24.9:
+            resultMessage = "Normal"
+        case 25.0...29.9:
+            resultMessage = "Weight above normal"
+        case 30...:
+            resultMessage = "Obesity"
+        default:
+            resultMessage = "Invalid value"
+        }
+        
+        // Create the alert controller
+        let alertController = UIAlertController(title: resultMessage, message: nil, preferredStyle: .alert)
+
+        alertController.addAction(okAction)
+        
+        // Present the alert
+                present(alertController, animated: true, completion: nil)
     }
+    
 }
 
